fa2c35befbcf93307f99165d78257bd154eb30f1
GiselleSerate <giselle.serate@gmail.com>
2016-11-12 01:42:16 -0800

    Added the proper grade levels to index.php because no one is making this site in first grade apparently

--- index.php
+++ index.php
@@ -19,8 +19,14 @@
 			Grade: 
 			<select name="grade" required>
 			  <option value="">Select...</option>
-			  <option value="1">1</option>
-			  <option value="2">2</option>
+			  <option value="5">5</option>
+			  <option value="6">6</option>
+			  <option value="7">7</option>
+			  <option value="8">8</option>
+			  <option value="9">9</option>
+			  <option value="10">10</option>
+			  <option value="11">11</option>
+			  <option value="12">12</option>
 			</select>
 			<br>
 			Password: 
